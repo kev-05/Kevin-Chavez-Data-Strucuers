65da5a0707a72d000f13c1ad027c65c19556f02b
kev-05 <165696942+kev-05@users.noreply.github.com>
2025-05-16 03:49:41 -0700

    Add files via upload

---
+++ Assignment 11.cpp
@@ -0,0 +1,39 @@
+//
+//  main.cpp
+//  Assignment 11
+//
+//  Created by Kevin Chavez on 5/16/25.
+//
+
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        // Last i elements are already in place
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap adjacent elements
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    // Choose one of the following:
+    // insertionSort(arr, n);
+    bubbleSort(arr, n);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    return 0;
+}
+
